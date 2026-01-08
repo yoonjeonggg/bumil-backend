2f26bb1a2a1aba3c4113ff62bb4c921504aab696
alvin081105 <ca081105@gmail.com>
2026-01-08 09:23:46 +0900

    태그 설명 작성

--- src/main/java/com/example/bumil_backend/entity/Tag.java
+++ src/main/java/com/example/bumil_backend/entity/Tag.java
@@ -1,8 +1,8 @@
 package com.example.bumil_backend.entity;
 
 public enum Tag {
-    IN_PROGRESS,
-    ADOPT,
-    REJECT,
-    END
+    IN_PROGRESS,    // 진행중
+    ADOPT,          // 채택
+    REJECT,         // 반려
+    END             // 종료
 }
