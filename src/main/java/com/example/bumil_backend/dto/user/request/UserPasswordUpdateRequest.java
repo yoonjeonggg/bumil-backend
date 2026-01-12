e6e374aab6f6fb97154b06f46253c011fabd5bd4
Knhye <knahye311@gmail.com>
2026-01-12 10:37:17 +0900

    fix: 회원 비밀번호 수정

--- src/main/java/com/example/bumil_backend/dto/user/request/UserPasswordUpdateRequest.java
+++ src/main/java/com/example/bumil_backend/dto/user/request/UserPasswordUpdateRequest.java
@@ -3,9 +3,10 @@ package com.example.bumil_backend.dto.user.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class UserPasswordUpdateRequest {
     @NotBlank
     private String currentPassword;
